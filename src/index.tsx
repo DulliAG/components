1e4529033201fcbd72f8b53a317d7a772c2b9096
Thorben <38816229+tklein1801@users.noreply.github.com>
2022-11-10 18:34:22 +0100

    save

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import Breadcrumb, { BreadcrumbProps } from './Breadcrumb/Breadcrumb';
-export { IToast, ToastContext, ToastContextProvider } from './Toast/Toast';
-export { BreadcrumbProps, Breadcrumb };
-export { NavbarLink, NavbarProps, Navbar } from './Navbar/Navbar';
-export {
-  DisclaimerProps as CookieDisclaimerProps,
-  Disclaimer as CookieDisclaimer,
-} from './Cookie/Cookie';
-export {
-  FooterLink,
-  FooterAd,
-  FooterAuthor,
-  FooterProps,
-  Footer,
-} from './Footer/Footer';
-export { SpinnerProps, Spinner } from './Spinner/Spinner';
+// import Breadcrumb, { BreadcrumbProps } from './Breadcrumb/Breadcrumb';
+// export { IToast, ToastContext, ToastContextProvider } from './Toast/Toast';
+// export { BreadcrumbProps, Breadcrumb };
+// export { NavbarLink, NavbarProps, Navbar } from './Navbar/Navbar';
+// export {
+//   DisclaimerProps as CookieDisclaimerProps,
+//   Disclaimer as CookieDisclaimer,
+// } from './Cookie/Cookie';
+// export {
+//   FooterLink,
+//   FooterAd,
+//   FooterAuthor,
+//   FooterProps,
+//   Footer,
+// } from './Footer/Footer';
+// export { SpinnerProps, Spinner } from './Spinner/Spinner';
+
+export { theme } from './theme';
