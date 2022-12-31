34883258f14e950b8a78d2c8c710f3ba4e790f82
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-01-01 00:30:53 +0100

    fix(types): Set correct setter-type

--- src/hooks/useStateCallback.hook.ts
+++ src/hooks/useStateCallback.hook.ts
@@ -1,15 +1,21 @@
 import React from 'react';
 
+export type PreviousState<T> = (previousState: T) => T;
+
+export type StateProps<T> = T | PreviousState<T>;
+
+export type StateCallback<S> = (updatedState: S) => void;
+
 /**
  * Author: https://stackoverflow.com/a/61842546
  */
 export function useStateCallback<T>(
   initialState: T
-): [T, (state: T, cb?: (state: T) => void) => void] {
+): [T, (state: StateProps<T>, cb?: StateCallback<T>) => void] {
   const [state, setState] = React.useState(initialState);
-  const cbRef = React.useRef<((state: T) => void) | undefined>(undefined); // init mutable ref container for callbacks
+  const cbRef = React.useRef<StateCallback<T> | undefined>(undefined); // init mutable ref container for callbacks
 
-  const setStateCallback = React.useCallback((state: T, cb?: (state: T) => void) => {
+  const setStateCallback = React.useCallback((state: StateProps<T>, cb?: StateCallback<T>) => {
     cbRef.current = cb; // store current, passed callback in ref
     setState(state);
   }, []); // keep object reference stable, exactly like `useState`
